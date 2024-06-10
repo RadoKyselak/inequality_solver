24803ceb73da700723316cc8e494de9d519e84f9
Rado Kyselak <142341444+RadoKyselak@users.noreply.github.com>
2024-06-10 16:13:50 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,18 +5,18 @@ def inequality_solver(a, b, c, x_range):
             answer.append(x)
     return answer if answer else "No Solution"
 
-a = float(input("A:"))
-b = float(input("B:"))
-c = float(input("C:"))
-range_required = input("Are you fine with the range being (0, 20)? y/n:")
+a = float(input("A: "))
+b = float(input("B: "))
+c = float(input("C: "))
+range_required = input("Are you fine with the range being (0, 20)? y/n: ")
 
 def use_range():
-    x_range = []
     if range_required == "n":
-        x_range_str = input("Enter a range in the format (n, n):")
-        return eval(x_range_str)
+        x_range_str = input("Enter a range in the format (n, n): ")
+        start, end = map(int, x_range_str.strip("()").split(","))
+        return range(start, end)
     else:
-        x_range = (0, 20)
+        return range(0, 20)
 
 x_range = use_range()
 
